a4cc051eae8bb5c8803202c2bd191b807d3516ff
Yamini1724 <yaminidhanamkala@gmail.com>
2025-04-30 21:08:20 +0530

    Strings

---
+++ Strings.py
@@ -0,0 +1,28 @@
+# ----- STRING -----
+
+a='yam'
+b="venu"
+c='''veni'''
+#print(type(a),type(b),type(c))
+
+# ------ STRING METHODS ------
+
+# UPPER()
+v='yamu'
+print(a.upper())
+
+#LOWER()
+y='VENU'
+print(y.lower())
+
+#endswith()
+x="yamu venu"
+print(x.endswith("venu"))
+
+#startswith()
+z="yamu venu"
+print(z.startswith("yamu"))
+
+#Replace
+u="yamu venu veni"
+print(u.replace("veni", "munnu"))
