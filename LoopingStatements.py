009f56dee556430b7b0d91a41eb38f05d7f4e4c2
Yamini1724 <yaminidhanamkala@gmail.com>
2025-04-30 13:17:58 +0530

    Looping Statements Practice

---
+++ LoopingStatements.py
@@ -0,0 +1,54 @@
+# for loop - here we use range - Uses for iterating over a sequence(like a list, tuple, string or range)
+
+# while loop - here we don't use range - it repeats a bloc of code as long as the given condition is true
+
+# nested loop - it means a loop inside another loop
+
+# --- FOR LOOP ---
+
+#for i in range(5):
+ #   print(i)
+
+#for i in range(0,5):
+   # print(i)
+
+#a=[1,2,3,56,250,545,2414]
+#for i in a:
+#    print(i)
+
+#v="VENI"
+#for i in v:
+#    print(i)
+
+
+
+# --- WHILE LOOP ---
+
+#i=0
+#while i<5:
+ #   print(i)
+
+#i=0
+#while i<5:
+    #print(i)
+    #i+=1
+
+#kiran=9
+#while kiran<15:
+ #   print("kiran")
+  #  kiran+=1
+
+
+# ---- Nested loop ----
+
+#for i in range(0,5):
+ #   for j in range(0,10):
+  #      print(i+j)
+
+i=0
+while i<3:
+    j=0
+    while j<2:
+        print(i,j)
+        j+=1
+i+=1
